4468d030ace449f62d4ba7cc8771809734280059
ACheungDSHS <62617067+ACheungDSHS@users.noreply.github.com>
2020-05-20 23:43:12 +0100

    Update Teacher.cs

--- learnCSharp/Teacher.cs
+++ learnCSharp/Teacher.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Transactions;
@@ -6,6 +6,10 @@ using System.Transactions;
 namespace learnCSharp
 {
     class Teacher : Person
+        public enum type
+        {
+            HEADOFDEPARTMENT, HEADTEACHER, HEADOFYEAR
+        };
     {
         String location;
         public Teacher(String name, String location) : base(name, Person.type.TEACHER)
